bbc7ee5bd04f7c9e91545a8f12e3ede6c97820ba
gtlevine <gtlevine@coastal.edu>
2023-03-19 22:15:29 -0400

    Week 9 Assignment

--- airports/views.py
+++ airports/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render # Import the render library to make loading templates easier
 from .models import Airport
 
 def index(request):
     # Fetch all airports from database
     airports = Airport.objects.all()
-    # Create a displayable string. We will change this next week.
-    # This is just to show some data.
-    airport_list = ', '.join([a.airport_code for a in airports])
-    return HttpResponse('Showing all airports: ' + airport_list);
+    # Place all airports into a context variable for retrieval in the view.
+    context = {'airports': airports}
+    return render(request, 'airports/index.html', context)
 
 def airport_info(request, airport_code):
     # Fetch the airport by a certain code
     # Remember as we are only expecting one airport per code we should use get
     airport = Airport.objects.get(airport_code=airport_code)
-    # Display the airport name and code
-    return HttpResponse('Showing info for airport: ' + airport.name + "- " + airport.airport_code)
+    context = {'airport': airport}
+    return render(request, 'airports/templates/airport.html', context)
